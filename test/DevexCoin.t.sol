da2062227dea79d0c261169bc2f287eb490c4b23
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-24 18:20:06 +0530

    added test approve function

--- test/DevexCoin.t.sol
+++ test/DevexCoin.t.sol
@@ -24,4 +24,22 @@ contract TestDevexCoin is Test {
         assertEq(c.balanceOf(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD), 100);
         assertEq(c.balanceOf(address(this)), 0);
     }
+
+    function test_Approve() public {
+        c.mint(address(this), 100);
+        c.approve(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD, 100);
+        uint amount = c.allowance(
+            address(this),
+            0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD
+        );
+        assertEq(amount, 100);
+        vm.prank(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD);
+        c.transferFrom(
+            address(this),
+            0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD,
+            100
+        );
+        assertEq(c.balanceOf(address(this)), 0);
+        assertEq(c.balanceOf(0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD), 100);
+    }
 }
